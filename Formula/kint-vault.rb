560c3a3fa108da3f72c4e17d51c23be04f2db116
Jozef Mikyta <mikyta.dev@gmail.com>
2026-03-22 23:02:47 +0100

    Fix checksums for v0.1.0

--- Formula/kint-vault.rb
+++ Formula/kint-vault.rb
@@ -7,22 +7,22 @@ class KintVault < Formula
   on_macos do
     on_arm do
       url "https://github.com/kint-pro/kint-vault-cli/releases/download/v0.1.0/kint-vault-cli_0.1.0_darwin_arm64.tar.gz"
-      sha256 "dd4e211042f7b2930eab30dcc9863949872734047326fbe8fad0eb4476a7c96e"
+      sha256 "7c8c5cef2e87255af5d3152244978865939e35ed980f89da0de9e6e0961a889a"
     end
     on_intel do
       url "https://github.com/kint-pro/kint-vault-cli/releases/download/v0.1.0/kint-vault-cli_0.1.0_darwin_amd64.tar.gz"
-      sha256 "65c5f4f64903086d4d9997f6b09427a45908ed6384f41d17dc3756dbb88e67a0"
+      sha256 "191ba1367db2fbdf8d82063949129ba575051de7a2844dfb4607ea0061cc896f"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/kint-pro/kint-vault-cli/releases/download/v0.1.0/kint-vault-cli_0.1.0_linux_arm64.tar.gz"
-      sha256 "de9ad601d4bbdadbd7e2c42e17d6578e2baf87a4ddf14d53a69ac9751f385ddf"
+      sha256 "ce45336b837c66bc30474f77ed6567485ca1e89df58617f62cf8c05f6f022e82"
     end
     on_intel do
       url "https://github.com/kint-pro/kint-vault-cli/releases/download/v0.1.0/kint-vault-cli_0.1.0_linux_amd64.tar.gz"
-      sha256 "3883c47fac7c5764b9387ea9d5ed047d34094d999e761a03ff2d215e0a858eba"
+      sha256 "3c295312b6205366f8c6bf7cccf479a591ee57b5d64d9cd776efca485693e46e"
     end
   end
 
