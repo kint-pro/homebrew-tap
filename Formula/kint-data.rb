5f210a6fe9314e0c30b3a4d04bd2956260ea3903
Jozef Mikyta <mikyta.dev@gmail.com>
2026-07-05 17:41:25 +0200

    fix: kint-data v0.1.0 checksums for rebuilt release

--- Formula/kint-data.rb
+++ Formula/kint-data.rb
@@ -9,22 +9,22 @@ class KintData < Formula
   on_macos do
     on_arm do
       url "https://github.com/kint-pro/kint-data-cli/releases/download/v0.1.0/kint-data-cli_0.1.0_darwin_arm64.tar.gz"
-      sha256 "ed54ec41c3625ec930b60d7a3692bc46905afd98d3504fdb7daf427535cd1ab6"
+      sha256 "f9eb3ea4e0e851d89a4673689e4c5b7829927a5bac8b3b0252d34aebb9c02f63"
     end
     on_intel do
       url "https://github.com/kint-pro/kint-data-cli/releases/download/v0.1.0/kint-data-cli_0.1.0_darwin_amd64.tar.gz"
-      sha256 "6a353ffa5640055caec154311fbd5b8b2ba08f2e73509dbfb394ccb28697926f"
+      sha256 "dd352e61763b77524a3589a35aa47c011d9ab4138b6dac416195d51c236582c8"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/kint-pro/kint-data-cli/releases/download/v0.1.0/kint-data-cli_0.1.0_linux_arm64.tar.gz"
-      sha256 "e819f3d18ad6f2ac7cf757ab51e8b50b33d332de45f1a2441ace3b336e20f8e2"
+      sha256 "3bda2ddc9a929c56aef003e095a87f653074d38859401d54577a1d0d895bf341"
     end
     on_intel do
       url "https://github.com/kint-pro/kint-data-cli/releases/download/v0.1.0/kint-data-cli_0.1.0_linux_amd64.tar.gz"
-      sha256 "146a743eb7fbe595fc79c1b7c22e0ff3506e25075a610d8e2503f670b2eaf8e1"
+      sha256 "824bbc2ac164f088826cf30c5909e72d5467c60afdc0e2c1acf8bd59a096dd8a"
     end
   end
 
